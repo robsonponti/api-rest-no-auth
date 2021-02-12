31cbc79a661f3ede776a034f9651e3ac6c8329f7
Robson Ponti <robsonponti@hotmail.com>
2021-02-12 11:51:48 +0000

    Created DB, Model, htaccess, index

---
+++ lib/DB.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace lib;
+
+class DB{
+
+    private static $db_host = "localhost";
+    private static $db_username = "root";
+    private static $db_password = "rob080594";
+    private static $db_name = "oskon_api";
+    protected static $conn;
+
+    public function __construct(){
+
+        try{
+
+            self::$conn = new \PDO("mysql:host= ".self::$db_host."; 
+            dbname= ".self::$db_name, 
+            self::$db_username, self::$db_password,
+            array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES UTF-8"));
+            self::$conn = setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$conn = setAttribute(PDO::ATTR_EMULATE, false);
+
+
+        }catch(Exception $e){
+            
+            exit($e->getMessage());
+        }
+
+        return self::conn;
+        
+    }
+
+
+
+
+}
---
+++ lib/Model.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace lib;
+
+require_once "DB.php";
+
+use lib\DB;
+
+class Model extends DB{
+
+    
+    public function __construct(){
+  
+        
+
+    }
+
+
+
+
+
+}
+
+
